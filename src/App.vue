dd452efa23aac03a040c0c7163e7e35e5540a785
Albert <=>
2020-04-21 09:08:19 +0700

    Inisiasi Project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700,900&display=swap');
+  body {
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="container">
+        <div class="header">
+            <hr>
+            <p class="brand">BLIRESS</p>
+            <p class="slogan">Blibli Employee Progress</p>
+            <hr>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    #container {
+        min-width: 100%;
+        min-height: 100vh;
+        background-image: url('../assets/bg-linear.svg');
+
+        .header {
+            font-family: 'Roboto';
+            padding: 4rem 2.5rem;
+            color: #FFF;
+            text-align: center;
+
+            hr {
+                border: 0.0625rem solid #FFF;
+            }
+
+            .brand {
+                margin: 0;
+                padding: 0;
+                font-size: 3em;
+                font-weight: 900;
+                text-shadow: 0.25rem 0.25rem 0.25rem rgba(0, 77, 133, 0.25);
+            }
+
+            .slogan {
+                font-size: 1.375em;
+                margin: 0.25rem 0 1rem;
+                padding: 0;
+                font-weight: 400;
+            }
+        }
+    }
+</style>
